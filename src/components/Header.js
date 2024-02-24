913ca071cca974db139bb4e00ebee8c0e49e2fa1
Rajesh Goud <rajeshgoud2314@gmail.com>
2024-02-24 17:31:41 +0530

    Your commit message here

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,7 +29,7 @@ function Header(){
             <img src={menu} alt="menu" className=' h-6 mx-4 mt-2 cursor-pointer  hidden sm:block' onClick={() => menuBarSet()}/>
             
             <a href='/'  alt="YouTube Home">
-            <img src={youtube} alt="youtube-icon" className=' h-5 ml-2 mt-[10px] cursor-pointer'/>
+            <img src={youtube} alt="youtube-icon" className='h-6 sm:h-5 ml-2 mt-[10px] cursor-pointer'/>
             </a>
 
         </div>
