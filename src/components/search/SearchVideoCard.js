de7e4f99eef4b77f8fec90caad2b5ef0e2c646e8
Rajesh Goud <rajeshgoud2314@gmail.com>
2024-02-24 18:09:11 +0530

    Your commit message here

--- src/components/search/SearchVideoCard.js
+++ src/components/search/SearchVideoCard.js
@@ -16,16 +16,16 @@ function SearchVideoCard({info}) {
         <Link to={id.videoId ? ('/watch?v='+ id.videoId ) : '?q=telugusongs'} className='flex ml-3 sm:m-6 sm:ml-0 flex-col sm:flex-row items-start w-[100%]'>
         <img
             src={thumbnails.high.url}
-            className='  min-h-40 h-[200px] w-[342px] sm:h-52 sm:w-96 rounded-xl object-cover transition-all duration-300 ease-in-out hover:rounded-none sm:mr-3' 
+            className='  min-h-40 h-[196px] w-[342px] sm:h-52 sm:w-96 rounded-xl object-cover transition-all duration-300 ease-in-out hover:rounded-none sm:mr-3' 
           />
           <div>
             
-          <h1 className=' text-sm font-semibold line-clamp-2 sm:text-lg text-gray-700'>{title}</h1>
+          <h1 className=' text-sm font-semibold line-clamp-2 sm:text-lg text-gray-700 px-2'>{title}</h1>
           <div className='flex items-center'>
             <img src={user} className=' h-8 '/>
             <p className=' text-sm ml-1  text-gray-700'>{channelTitle}</p>
           </div>
-          <p className=' max-w-2xl text-xs'> {description}</p>
+          <p className=' max-w-2xl text-xs pr-2'> {description}</p>
           </div></Link>
           
     </div>
