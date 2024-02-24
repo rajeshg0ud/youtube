75b54a542f47a71cf2a316e226685f96e17d165c
Rajesh Goud <rajeshgoud2314@gmail.com>
2024-02-24 17:09:42 +0530

    Your commit message here

--- src/components/search/SearchVideoCard.js
+++ src/components/search/SearchVideoCard.js
@@ -13,7 +13,7 @@ function SearchVideoCard({info}) {
         <Link to={id.videoId ? ('/watch?v='+ id.videoId ) : '?q=telugusongs'} className='flex m-6 flex-col sm:flex-row items-start '>
         <img
             src={thumbnails.high.url}
-            className='  min-h-40 h-52 w-96 sm:h-52 sm:w-96 rounded-xl object-cover transition-all duration-300 ease-in-out hover:rounded-none sm:mr-2' 
+            className='  min-h-40 h-52 w-[346px] sm:h-52 sm:w-96 rounded-xl object-cover transition-all duration-300 ease-in-out hover:rounded-none sm:mr-2' 
           />
           <div>
             
