a0f90acf829cd8b5785d3e8c83fc28a9c49abd5b
Rajesh Goud <rajeshgoud2314@gmail.com>
2024-02-24 16:18:57 +0530

    Your commit message here

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ function Header(){
   return (
     <div className=' fixed grid grid-flow-col p-3 shadow-lg w-full z-10  bg-white top-0 left-0 items-center'>
         <div className='flex col-span-1'>
-            <img src={menu} alt="menu" className=' h-6 w-6 mx-4 mt-2 cursor-pointer  hidden sm:block' onClick={() => menuBarSet()}/>
+            <img src={menu} alt="menu" className=' h-6 min-w-[25%] mx-4 mt-2 cursor-pointer  hidden sm:block' onClick={() => menuBarSet()}/>
             
             <a href='/'  alt="YouTube Home">
             <img src={youtube} alt="youtube-icon" className=' h-5 ml-2 mt-[10px] cursor-pointer'/>
@@ -38,7 +38,7 @@ function Header(){
         <div className='flex justify-center col-span-10 ' >
 
         <form  onSubmit={handleSubmit} className='w-9/12 sm:w-6/12 flex justify-center '>
-        <input type='text' placeholder='Search' className=' w-7/12 sm:w-4/5 py-2 h-[30px] sm:h-[38px]  pl-4 border border-gray-500 rounded-l-full' onChange={(e)=>setSearchQuery(e.target.value)}/>
+        <input type='text' placeholder='Search' className=' w-6/12 sm:w-4/5 py-2 h-[30px] sm:h-[38px]  pl-4 border border-gray-500 rounded-l-full' onChange={(e)=>setSearchQuery(e.target.value)}/>
          <button className=' px-1 border py-2 sm:px-4 border-gray-500 rounded-r-full  h-[30px] sm:h-[38px]  bg-gray-100 hover:bg-gray-200 self-center' >
         <AiOutlineSearch size={25}  className='pb-1' /></button> 
         </form>
